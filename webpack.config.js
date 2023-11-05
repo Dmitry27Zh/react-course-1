4cd774f38fc3374fb720b07ee74360b99a29d9de
Dmitry <dmitry27zh@gmail.com>
2023-11-05 12:37:11 +0500

    Fix webpack config and start dev script

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ let path = require('path')
 let MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 let conf = {
-  entry: './src/main.js',
+  entry: './src/main.tsx',
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: 'main.js',
