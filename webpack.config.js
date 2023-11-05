28923b5b5fba89afecb2bbbf16db78038c6e71bd
Dmitry <dmitry27zh@gmail.com>
2023-11-05 12:30:57 +0500

    Add TS

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,45 @@
-let path = require('path');
-let MiniCssExtractPlugin = require('mini-css-extract-plugin');
+let path = require('path')
+let MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 let conf = {
-	entry: './src/main.js',
-	output: {
-		path: path.resolve(__dirname, './dist'),
-		filename: 'main.js',
-		publicPath: '/dist/'
-	},
-	devServer: {
-		static: {
-			directory: path.join(__dirname, '.'),
-		}
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				loader: 'babel-loader',
-				exclude: '/node_modules/'
-			},
-			{
-				test: /\.css$/,
-				use: [MiniCssExtractPlugin.loader, 'css-loader']
-			}
-		]
-	},
-	plugins: [ 
-		new MiniCssExtractPlugin({
-			filename: 'main.css'
-		})
-	]
-};
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'main.js',
+    publicPath: '/dist/',
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, '.'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: '/node_modules/',
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: '/node_modules/',
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'main.css',
+    }),
+  ],
+}
 
 module.exports = (env, options) => {
-	let isProd = options.mode === 'production';
-	conf.devtool = isProd ? false : 'eval-cheap-module-source-map';
-	return conf;
+  let isProd = options.mode === 'production'
+  conf.devtool = isProd ? false : 'eval-cheap-module-source-map'
+  return conf
 }
