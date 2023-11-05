a37529d2181f65fe368f7b2562cda7aa6147c973
Dmitry <dmitry27zh@gmail.com>
2023-11-05 13:41:27 +0500

    JSX

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,28 @@
+import React from 'react'
 import ReactDom from 'react-dom'
 
-ReactDom.render(<div>Hello, World!</div>, document.querySelector('.app') as HTMLElement)
+function UserCard(props: { name: string; text: string }) {
+  return (
+    <>
+      <div className="card">
+        <h3>{props.name}</h3>
+        <hr />
+        <div>{props.text}</div>
+      </div>
+      <hr />
+    </>
+  )
+}
+
+ReactDom.render(
+  <div className="some">
+    <h2>Hello, World!</h2>
+    <hr />
+    <UserCard name="Dmitry" text="Hi there!" />
+    <hr />
+    <div>Text</div>
+  </div>,
+  document.querySelector('.app') as HTMLElement
+)
+
+React.createElement(UserCard, { name: 'Dmitry', text: 'Hi there!' })
