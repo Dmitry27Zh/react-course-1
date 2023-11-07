091de28cee51f4b3b4f1e2657fd35f45ec2feb75
Dmitry <dmitry27zh@gmail.com>
2023-11-07 12:29:07 +0500

    Refactor Counter

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,28 +1,64 @@
-import React from 'react'
+import React, { ChangeEvent, useEffect, useState, useCallback, useMemo } from 'react'
+import { debounce } from './utils'
+import { COUNTER_TIMEOUT } from './constants'
 
 type Props = {
+  min?: number
   max: number
 }
 
-export default class extends React.Component<Props> {
-  state = {
-    current: 1,
+export default function ({ min = 0, max }: Props): JSX.Element | never {
+  if (min > max) {
+    throw new Error('Min should be less than max')
   }
 
-  increment = () => {
-    if (this.state.current < this.props.max) {
-      this.setState({ current: this.state.current + 1 })
+  const [current, setCurrent] = useState<number>(min)
+  const [input, setInput] = useState<string>(String(min))
+  useEffect(() => {
+    setInput(String(current))
+  }, [current])
+  const applyCurrent = (value: number) => {
+    if (value >= min && value <= max) {
+      setCurrent(value)
+      return true
     }
+
+    return false
+  }
+  const decrement = () => {
+    applyCurrent(current - 1)
+  }
+  const increment = () => {
+    applyCurrent(current + 1)
+  }
+  const updateCurrent = useMemo<(input: string, current: number) => void>(() => {
+    return debounce((input: string, current: number) => {
+      let isApplied = false
+
+      if (input) {
+        isApplied = applyCurrent(Number(input))
+      }
+
+      if (!isApplied) {
+        setInput(String(current))
+      }
+    }, COUNTER_TIMEOUT)
+  }, [])
+  const handleChange = ({ target }: ChangeEvent<HTMLInputElement>) => {
+    const { value } = target
+    setInput(value)
+    updateCurrent(value, current)
   }
 
-  render(): JSX.Element {
-    return (
-      <div>
-        <span>{this.state.current}</span>
-        <button type="button" onClick={this.increment}>
-          +
-        </button>
-      </div>
-    )
-  }
+  return (
+    <div>
+      <button type="button" onClick={decrement}>
+        -
+      </button>
+      <input type="text" value={input} onChange={handleChange} />
+      <button type="button" onClick={increment}>
+        +
+      </button>
+    </div>
+  )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import UserCard from './components/UserCard'
 import Counter from './components/Counter'
-import CounterFn from './components/CounterFn'
 
 ReactDom.render(
   <div className="some">
@@ -12,13 +11,9 @@ ReactDom.render(
     <hr />
     <div>Text</div>
     <h3>Class max=3</h3>
-    <Counter max={3} />
-    <hr />
-    <h3>Class max=5</h3>
-    <Counter max={5} />
     <hr />
     <h3>Function min=0 max=2</h3>
-    <CounterFn min={0} max={2} />
+    <Counter min={0} max={2} />
   </div>,
   document.querySelector('.app') as HTMLElement
 )
