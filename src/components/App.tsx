7a069e036ca83f7f8c93366425b383dd1f74d607
Dmitry <dmitry27zh@gmail.com>
2023-11-17 11:04:50 +0500

    Refactor App

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,7 +3,7 @@ import Cart from './Cart'
 import Order from './Order'
 import Result from './Result'
 import { observer } from 'mobx-react-lite'
-import useStore from '../hooks/useStore'
+import Footer from './Footer'
 
 export default observer(App)
 
@@ -12,34 +12,15 @@ function App(): JSX.Element {
   const moveToCart = () => setPage('cart')
   const moveToOrder = () => setPage('order')
   const moveToResult = () => setPage('result')
-  const { settings: SettingsContext } = useStore()
-  const { settings, changeTheme } = SettingsContext
 
   return (
     <>
       <div className="container mt-5 mb-5 pd-4">
-        {page === 'cart' && <Cart onNext={moveToOrder} settings={settings} />}
+        {page === 'cart' && <Cart onNext={moveToOrder} />}
         {page === 'order' && <Order onNext={moveToResult} onPrev={moveToCart} />}
         {page === 'result' && <Result />}
       </div>
-      <footer className="container pt-4 pb-4">
-        <div className="d-flex gap-2">
-          <button
-            className={`btn btn-light ${settings.theme === 'light' ? 'active' : ''}`}
-            type="button"
-            onClick={() => changeTheme('light')}
-          >
-            Switch to light
-          </button>
-          <button
-            className={`btn btn-dark ${settings.theme === 'dark' ? 'active' : ''}`}
-            type="button"
-            onClick={() => changeTheme('dark')}
-          >
-            Switch to dark
-          </button>
-        </div>
-      </footer>
+      <Footer />
     </>
   )
 }
