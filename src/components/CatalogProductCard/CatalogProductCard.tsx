0903c8cc9f13a25c5075f86cd906a88a38d03d29
Dmitry <dmitry27zh@gmail.com>
2023-11-20 12:02:55 +0500

    Add Link in CatalogProductCard

--- src/components/CatalogProductCard/CatalogProductCard.tsx
+++ src/components/CatalogProductCard/CatalogProductCard.tsx
@@ -1,6 +1,7 @@
 import { observer } from 'mobx-react-lite'
 import useStore from '../../hooks/useStore'
 import { Props } from './props'
+import { Link } from 'react-router-dom'
 
 export default observer(CatalogProductCard)
 
@@ -26,7 +27,10 @@ export function CatalogProductCard({ id, price, title }: Props) {
     <div className="p-4 g-col-4 w-25 flex-grow-1 border border-1 rounded-2">
       <h3>{title}</h3>
       <p>Price: {price}</p>
-      {renderButton()}
+      <div className="d-flex align-items-center gap-3">
+        <Link to={`products/${id}`}>Open</Link>
+        {renderButton()}
+      </div>
     </div>
   )
 }
