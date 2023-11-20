f52084477779f792cfe995bd8c4987ea145dde6e
Dmitry <dmitry27zh@gmail.com>
2023-11-20 13:19:36 +0500

    Add available link state

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,20 +1,29 @@
 import { Link } from 'react-router-dom'
+import useStore from '../../hooks/useStore'
 
 export default function () {
+  const { cart } = useStore()
   const links = [
-    { text: 'Home', path: '/' },
-    { text: 'Cart', path: '/cart' },
-    { text: 'Order', path: '/order' },
+    { text: 'Home', path: '/', available: true },
+    { text: 'Cart', path: '/cart', available: true },
+    { text: 'Order', path: '/order', available: !cart.isEmpty },
     { text: 'Result', path: '/result' },
   ]
-
   return (
     <div className="d-flex flex-column">
-      {links.map((link, index) => (
-        <Link key={`${index}-${link.text}`} to={link.path}>
-          {link.text}
-        </Link>
-      ))}
+      {links.map((link, index) => {
+        const key = `${index} ${link.text} hoho`
+
+        if (link.available) {
+          return (
+            <Link key={key} to={link.path}>
+              {link.text}
+            </Link>
+          )
+        } else {
+          return <span key={key}>{link.text}</span>
+        }
+      })}
     </div>
   )
 }
